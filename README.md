6270de6f8b21857008093a49deffb42667519c4a
侯孝明 <2559831076@qq.com>
2024-09-30 09:55:27 +0800

    feat: init

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# vue-bs-temp
+# ll
 
 This template should help get you started developing with Vue 3 in Vite.
 
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <h1 mt20>
+      aas
+    </h1>
+    <TheWelcome />
+  </main>
+</template>
